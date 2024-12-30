b6ef08e6f9226bfb00780a58b9bd43ffa599ef26
simowilso <siwilson@hotmail.com>
2024-12-30 11:05:01 +0000

    Update ECU1.ino

--- Software/ECU1/ECU1/ECU1.ino
+++ Software/ECU1/ECU1/ECU1.ino
@@ -236,7 +236,7 @@ uint8_t counterExercise = 0;
 
   // /*replaceValueHere*/ uint8_t receiverAddress1[] = { 0x40, 0xF5, 0x20, 0x33, 0xDA, 0xF9 };    //  this ECU MAC address ,only for example purposes
   /*replaceValueHere*/ uint8_t receiverAddress2[] = { 0xEC, 0xFA, 0xBC, 0x01, 0xA0, 0xA8 };  //  ECU 2
-/*replaceValueHere*/ uint8_t receiverAddress3[] = { 0xAC, 0x0B, 0xFB, 0xCF, 0xD8, 0xB1 };    //  ECU 3
+/*replaceValueHere*/ uint8_t receiverAddress3[] = { 0x3C, 0x61, 0x05, 0xED, 0x2A, 0xB6 };    //  ECU 3
 /*replaceValueHere*/ uint8_t receiverAddress4[] = { 0xF4, 0xCF, 0xA2, 0x79, 0x23, 0x84 };    //  ECU 4
 // /*replaceValueHere*/ uint8_t receiverAddress4[] = { 0x4C, 0xEB, 0xD6, 0x62, 0x09, 0x54 };    //  ECU 5
 
